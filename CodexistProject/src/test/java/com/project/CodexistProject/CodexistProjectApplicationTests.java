9c518e995a2f0919e325b43ef4bf50b219426c6b
150202078@kocaeli.edu.tr <150202078@kocaeli.edu.tr>
2022-09-22 23:48:47 +0300

    First Commit. Proje Oluşturuldu.

---
+++ CodexistProject/src/test/java/com/project/CodexistProject/CodexistProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.CodexistProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodexistProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
